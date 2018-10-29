8ee883294651cae7e8d391ba9e991f689b9856e5
Avanade <Avanade@WIN-NCCOJBMMAH.corp.avanade.org>
2018-10-29 20:58:24 -0300

    Exibicao das habilidades

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
 const qs = document.querySelector.bind(document);
 const qsa = document.querySelectorAll.bind(document);
 
-
 // Ref para o botao de busca
-const button = qs('.button');
+const button = qs('#submit');
 button.addEventListener('click', getPokemon)
 
 function getPokemon (e) {
   e.preventDefault();
 
+  // valor digitado no input
   const {value} = qs('#pokemon');
   $.getJSON(`https://pokeapi.co/api/v2/pokemon/${value}`, exibeInformacoesDoPokemon);
 }
 
 function exibeInformacoesDoPokemon(data) {
-  console.log(data);
+  exibeHabilidades(data.abilities);
 } 
+
+function exibeHabilidades(habilidades) {
+  const listaDeHabilidades = habilidades.reduce((prev, curr) => prev.concat(`<p> ${curr.ability.name} </p>`), '')
+  qs('#poke-abilities').innerHTML = listaDeHabilidades;
+}
